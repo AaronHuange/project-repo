6c473d7de7b4eef4dd551030d2461d36f0c48306
Aaron <aaronhuange@gmail.com>
2023-06-01 22:33:35 +0800

    where判断

--- ask.js
+++ ask.js
@@ -23,7 +23,7 @@ module.exports = {
       question: '需要连接数据库'
     },
     {
-      where: 'return params.SQL',
+      where: 'SQL',
       type: 'input',
       field: 'SQL_HOST',
       question: '数据库地址'
