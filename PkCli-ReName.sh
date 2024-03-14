359bbab0066dfc9cc0c87a292717c484dead1740
Aaron <aaronhuange@gmail.com>
2024-03-14 19:25:06 +0800

    提交模版合并代码

---
+++ PkCli-ReName.sh
@@ -0,0 +1,13 @@
+# 合并前
+mv ./project.tt.json ./project.tt.json.ejs
+mv ./project.config.json ./project.config.json.ejs
+mv ./package.json ./package.json.ejs
+mv ./src/index.html ./src/index.html.ejs
+mv ./config/index.js ./config/index.js.ejs
+
+# 合并后
+#mv ./project.tt.json.ejs ./project.tt.json
+#mv ./project.config.json.ejs ./project.config.json
+#mv ./package.json.ejs ./package.json
+#mv ./src/index.html.ejs ./src/index.html
+#mv ./config/index.js.ejs ./config/index.html
